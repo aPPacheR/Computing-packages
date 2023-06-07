2076a554b0cf9e4a19a64d623f6233e33318f351
aPPacheR <87997793+aPPacheR@users.noreply.github.com>
2023-06-08 01:19:34 +0300

    Update checkerboard.m

--- Octave/checkerboard.m
+++ Octave/checkerboard.m
@@ -1,1 +1,15 @@
-function res = checkerboard(rows, cols)    if nargin < 2        cols = rows;    end    res = true(rows, cols);    for i = 1:rows      for j = 1:cols        if mod(i, 2) != 0 && mod(j, 2) == 0          res(i, j) = false;        elseif mod(i, 2) == 0 && mod(j, 2) != 0          res(i, j) = false;        end      end    endend
+function res = checkerboard(rows, cols)
+    if nargin < 2
+        cols = rows;
+    end
+    res = true(rows, cols);
+    for i = 1:rows
+      for j = 1:cols
+        if mod(i, 2) != 0 && mod(j, 2) == 0
+          res(i, j) = false;
+        elseif mod(i, 2) == 0 && mod(j, 2) != 0
+          res(i, j) = false;
+        end
+      end
+    end
+end
